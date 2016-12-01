5c8573dc0e599aed769a6ad9943836ce425409bd
Paul Landes <landes@mailc.net>
2016-12-01 11:14:03 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,36 @@
+(defproject com.zensols.nlp/gate "0.1.0-SNAPSHOT"
+  :description "Wrapper for Gate Annotation Utility"
+  :url "https://github.com/plandes/Gate wraper"
+  :license {:name "Apache License version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :plugins [[lein-codox "0.10.1"]
+            [org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
+  :codox {:metadata {:doc/format :markdown}
+          :project {:name "Gate wraper"}
+          :output-path "target/doc/codox"}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :javac-options ["-Xlint:unchecked"]
+  :jar-exclusions [#".gitignore"]
+  :exclusions [org.slf4j/slf4j-log4j12
+               ch.qos.logback/logback-classic]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 ;; logging
+                 [org.clojure/tools.logging "0.3.1"]
+
+                 ;; command line
+                 [com.zensols.tools/actioncli "0.0.11"]
+
+                 ;; gate
+                 [uk.ac.gate/gate-core "8.2"]]
+  :profiles {:appassem {:aot :all}
+             :dev
+             {:jvm-opts
+              ["-Dlog4j.configurationFile=test-resources/log4j2.xml" "-Xms4g" "-Xmx12g" "-XX:+UseConcMarkSweepGC"]
+              :dependencies [[com.zensols/clj-append "1.0.4"]
+                             [org.apache.logging.log4j/log4j-core "2.3"]
+                             [org.apache.logging.log4j/log4j-api "2.3"]
+                             [org.apache.logging.log4j/log4j-slf4j-impl "2.3"]
+                             [org.apache.logging.log4j/log4j-jcl "2.3"]]}})
---
+++ src/clojure/zensols/annotate/gate.clj
@@ -0,0 +1,136 @@
+(ns zensols.annotate.gate
+  (:import [java.util HashSet]
+           [gate Gate Factory]
+           [gate.corpora DocumentImpl DocumentContentImpl]
+           [gate.creole AnnotationSchema FeatureSchema])
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log])
+  (:require [zensols.actioncli.resource :as res]))
+
+(defn initialize
+  "Initialize the Gate system.  This is called when this namespace is loaded."
+  []
+  (res/register-resource :gate-home :system-file "gate.home"
+                         :system-default
+                         (-> (System/getProperty "user.home")
+                             (io/file "Applications/GateDeveloper")
+                             .getAbsolutePath))
+  (let [gate-home (res/resource-path :gate-home)]
+    (log/infof "initializing gate system with home: %s" gate-home)
+    (-> (io/file gate-home "plugins")
+        .getAbsolutePath
+        (#(System/setProperty "gate.plugins.home" %)))
+    (-> (io/file gate-home "gate.xml")
+        .getAbsolutePath
+        (#(System/setProperty "gate.site.config" %)))
+    (Gate/init)
+    gate-home))
+
+(defn configure-plugins
+  "Configure Gate plugins.  The no-arg default configures the `Alignment`
+  plugin."
+  ([]
+   (configure-plugins ["Alignment"]))
+  ([plugins]
+   (doseq [plugin plugins]
+     (let [plugin-dir (res/resource-path :gate-home
+                                         (format "plugins/%s" plugin))]
+       (-> (Gate/getCreoleRegister)
+           (.registerDirectories (io/as-url plugin-dir)))))))
+
+(defn- delete-recursively [fname]
+  (let [func (fn [func f]
+               (when (.isDirectory f)
+                 (doseq [f2 (.listFiles f)]
+                   (func func f2)))
+               (io/delete-file f))]
+    (func func (io/file fname))))
+
+(defn- feature-map
+  "Create a Gate `FeatureMap` from a Clojure (or any) map."
+  [map]
+  (doto (Factory/newFeatureMap)
+    (.putAll map)))
+
+(defn annotation-schema-from-resource
+  "Create a schema annotation from a schema the contents of **resource**.
+  See [Gate docs](https://gate.ac.uk/sale/tao/splitch7.html#x11-1720007.11)).
+  See [[annotation-schema]]."
+  [resource]
+  (->> {"xmlFileUrl" (->> resource io/resource io/as-url)}
+       feature-map
+       (Factory/createResource "gate.creole.AnnotationSchema")))
+
+(defn annotation-schema
+  "Create an annotation schema (i.e. entity) **label**.  If **options** is
+  given provide additional feature schema metadata.
+  See [[annotation-schema-from-resource]]."
+  ([label]
+   (annotation-schema label nil))
+  ([label options]
+   (->> (doto (AnnotationSchema.)
+          (.setAnnotationName label)
+          (.setFeatureSchemaSet
+           (->> options
+                (map (fn [{:keys [name value use options]
+                           :or {use :default}}]
+                       (let [usestr (cond (= use :default) "default"
+                                          (= use :fixed) "fixed"
+                                          true "")]
+                         (FeatureSchema. name String
+                                         (or value (format "%s-value" name))
+                                         usestr
+                                         (if options
+                                           (HashSet. (set options)))))))
+                HashSet.))))))
+
+(defn create-document
+  "Create a document with raw text.  You can annotate the returned document
+  with [[annotate-document]]."
+  [text]
+  (doto (DocumentImpl.)
+    (.setContent (DocumentContentImpl. text))))
+
+(defn annotate-document
+  "Annotate a document with entity **label** `type` from character
+  position [**start** **end**) using additional entity metadata **features** in
+  document **doc**."
+  [start end label features doc]
+  (let [fmap (.getFeatures doc)
+        anons (.getAnnotations doc)]
+    (.add anons start end label (feature-map features))
+    doc))
+
+(defn store-documents
+  "Create a Gate data store that can be opened by the Gate GUI.  This creates a
+  directory structure at **store-dir** and populates it **documents** that were
+  create with [[create-document]].
+
+  **Important**: this first deletes the **store-dir** directory if it exists.
+
+Keys
+----
+* **corpus-name**: names the corpus and defaults to `zensos-gate-corpus`
+* **resources**: resources (i.e. entities created with [[annotation-schema]])"
+  [store-dir documents
+   & {:keys [corpus-name resources]
+      :or {corpus-name "zensols-gate-corpus"}}]
+  (if (.exists store-dir)
+    (delete-recursively store-dir))
+  (let [store (Factory/createDataStore "gate.persist.SerialDataStore"
+                                       (-> store-dir io/as-url .toString))]
+    (try
+      (let [corpus (Factory/newCorpus corpus-name)
+            feats (doto (Factory/newFeatureMap)
+                    (.put "transientSource" corpus))
+            cd-feats (Factory/newFeatureMap)]
+        (doall (map #(.add corpus %) documents))
+        (->> (.adopt store corpus)
+             (.sync store))
+        (doseq [res resources]
+          (->> (.adopt store res)
+               (.sync store)))
+        (log/infof "wrote store at: %s" store-dir))
+      (finally (.close store)))))
+
+(initialize)
